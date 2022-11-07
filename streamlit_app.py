ce5882b91027bea1f5ec08afff111f64f2150917
cooolbabu <cooolbabu@yahoo.com>
2022-11-07 10:35:14 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ import streamlit;
 streamlit.title("Hello World");
 
 streamlit.title("Checking out integration with StreamLit")
+
+streamlit.header(' Breakfast Menu')
+streamlit.text(':chicken: Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
